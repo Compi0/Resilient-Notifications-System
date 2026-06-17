38a0bae4294c64faf2c0007494dcb2b7118f7f5f
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-17 11:33:18 -0600

    Add controller for main entry point

--- src/main/java/com/tromfi/notifications/adapters/in/rest/NotificationController.java
+++ src/main/java/com/tromfi/notifications/adapters/in/rest/NotificationController.java
@@ -1,4 +1,42 @@
-package com.tromfi.notifications.adapters.in;
+package com.tromfi.notifications.adapters.in.rest;
 
+import com.tromfi.notifications.adapters.in.rest.dto.NotificationRequestDTO;
+import com.tromfi.notifications.adapters.in.rest.dto.NotificationResponseDTO;
+import com.tromfi.notifications.adapters.in.rest.mapper.AckNotificationMapper;
+import com.tromfi.notifications.adapters.in.rest.mapper.NotificationMapper;
+import com.tromfi.notifications.application.usecase.NotificationOrchestrationService;
+import com.tromfi.notifications.application.usecase.command.CreateNotificationCommand;
+import com.tromfi.notifications.application.usecase.result.NotificationAck;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@AllArgsConstructor
 public class NotificationController {
+
+    private final NotificationOrchestrationService notificationOrchestrationService;
+
+    @PostMapping("/notification")
+    public ResponseEntity<NotificationResponseDTO> sendNotification(@RequestBody NotificationRequestDTO notificationRequestDTO) {
+
+
+        // TODO Aqui se haria algunas validaciones
+
+        CreateNotificationCommand notificationCommand = NotificationMapper.MAPPER.
+                toCreateNotificationCommand(notificationRequestDTO);
+
+        NotificationAck notificationAck = notificationOrchestrationService.sendNotification(notificationCommand);
+
+        NotificationResponseDTO notificationResponseDTO = AckNotificationMapper.MAPPER.
+                toNotificationResponseDTO(notificationAck);
+
+        // Recordar eso del httpstatus, no recuerdo si dependia de esto o como se hacia si se tuvo un error?
+        // ControllerAdvice
+        return new ResponseEntity<>(notificationResponseDTO, HttpStatus.ACCEPTED);
+    }
+
 }
