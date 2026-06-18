7aacbac6e334777fbbcfeed535d952cc6d34170c
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-17 20:27:49 -0600

    Add configuration to use ThreadPoolTaskExecutor as base for the worker

--- src/main/java/com/tromfi/notifications/infraestructure/AsyncConfig.java
+++ src/main/java/com/tromfi/notifications/infraestructure/AsyncConfig.java
@@ -1,4 +1,26 @@
 package com.tromfi.notifications.infraestructure;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
 public class AsyncConfig {
+
+    @Bean(name = "backgroundTaskExecutor")
+    public Executor taskExecutor() {
+
+        System.out.println("Background task executor started");
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(30);
+        executor.setThreadNamePrefix("BgService-");
+        executor.initialize();
+        return executor;
+    }
+
 }
