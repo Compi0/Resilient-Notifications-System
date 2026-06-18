2059ba75138963c0b7a455a1e308a24b313ca582
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-17 20:30:56 -0600

    Initial creation of mappers

--- src/main/java/com/tromfi/notifications/adapters/in/rest/mapper/AckNotificationMapper.java
+++ src/main/java/com/tromfi/notifications/adapters/in/rest/mapper/AckNotificationMapper.java
@@ -1,4 +1,15 @@
 package com.tromfi.notifications.adapters.in.rest.mapper;
 
-public class AckNotificationMapper {
+import com.tromfi.notifications.adapters.in.rest.dto.NotificationResponseDTO;
+import com.tromfi.notifications.application.usecase.result.NotificationAck;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AckNotificationMapper {
+
+    AckNotificationMapper MAPPER = Mappers.getMapper(AckNotificationMapper.class);
+
+    NotificationResponseDTO toNotificationResponseDTO(NotificationAck notification);
+
 }
--- src/main/java/com/tromfi/notifications/adapters/in/rest/mapper/NotificationMapper.java
+++ src/main/java/com/tromfi/notifications/adapters/in/rest/mapper/NotificationMapper.java
@@ -1,4 +1,18 @@
 package com.tromfi.notifications.adapters.in.rest.mapper;
 
+import com.tromfi.notifications.adapters.in.rest.dto.NotificationRequestDTO;
+import com.tromfi.notifications.application.usecase.command.CreateNotificationCommand;
+import com.tromfi.notifications.domain.model.Notification;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface NotificationMapper {
+
+    NotificationMapper MAPPER = Mappers.getMapper(NotificationMapper.class);
+
+    //@Mapping() <- Esto es por si hay diferentes nombres de los properties, sepa cual es a la que se refiere
+    CreateNotificationCommand toCreateNotificationCommand(NotificationRequestDTO notification);
+
 }
