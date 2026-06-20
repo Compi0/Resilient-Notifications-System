c575d5f5f55530bf62f953baee59d445e624964c
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-19 23:47:59 -0600

    Add initial scheleton for domain service

--- src/main/java/com/tromfi/notifications/domain/service/ProcessNotificationService.java
+++ src/main/java/com/tromfi/notifications/domain/service/ProcessNotificationService.java
@@ -1,4 +1,38 @@
 package com.tromfi.notifications.domain.service;
 
-public class processNotificationService {
+import com.tromfi.notifications.domain.model.Notification;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProcessNotificationService {
+
+    public boolean validateNotification(Notification notification) {
+
+        if (notification == null) {
+            return false;
+        }
+        return notification.isRetryingAvailable();
+    }
+
+    public Notification acceptNotification(Notification notification) {
+
+        notification.markSent();
+
+        return notification;
+    }
+
+    public Notification rejectNotification(Notification notification) {
+
+        notification.calculateNextAttempt();
+
+        return notification;
+    }
+
+    public Notification sendToDeadQueue(Notification notification) {
+
+        // Todavia pensar como se va a aplicar
+
+        return notification;
+    }
+
 }
