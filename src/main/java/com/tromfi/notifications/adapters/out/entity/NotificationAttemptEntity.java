bd47059a6dccadb8d016110752c7d08cb40d7596
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-22 21:13:16 -0600

    Initial implementation of NotificationAttemptEntity

--- src/main/java/com/tromfi/notifications/adapters/out/entity/NotificationAttemptEntity.java
+++ src/main/java/com/tromfi/notifications/adapters/out/entity/NotificationAttemptEntity.java
@@ -1,4 +1,29 @@
 package com.tromfi.notifications.adapters.out.entity;
 
+import com.tromfi.notifications.domain.model.enums.AuditState;
+import com.tromfi.notifications.domain.model.enums.ProviderMessage;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class NotificationAttemptEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_notification_id", referencedColumnName = "id")
+    private NotificationEntity notificationEntity;
+
+    private LocalDateTime timestampAttempt;
+    private String providerMessage;
+    private String providerErrorCode;
+    private String status;
+
 }
--- src/main/java/com/tromfi/notifications/adapters/out/entity/mapper/NotificationAttemptEntityMapper.java
+++ src/main/java/com/tromfi/notifications/adapters/out/entity/mapper/NotificationAttemptEntityMapper.java
@@ -1,10 +1,40 @@
-package com.tromfi.notifications.adapters.out.entity;
+package com.tromfi.notifications.adapters.out.entity.mapper;
 
+import com.tromfi.notifications.adapters.out.entity.NotificationAttemptEntity;
+import com.tromfi.notifications.adapters.out.entity.NotificationEntity;
+import com.tromfi.notifications.domain.model.NotificationAttempt;
+import com.tromfi.notifications.domain.model.enums.AuditState;
+import com.tromfi.notifications.domain.model.enums.ProviderMessage;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuditEntityMapper {
+public class NotificationAttemptEntityMapper {
 
+    public NotificationAttemptEntity toNotificationAttemptEntity(NotificationAttempt notificationAttempt) {
+        NotificationAttemptEntity notificationAttemptEntity = new NotificationAttemptEntity();
 
+        NotificationEntity notificationEntity = new NotificationEntity(notificationAttempt.getNotificationId());
+
+        //notificationAttemptEntity.setId(notificationAttempt.getId());
+        notificationAttemptEntity.setNotificationEntity(notificationEntity);
+        notificationAttemptEntity.setTimestampAttempt(notificationAttempt.getTimestampAttempt());
+        notificationAttemptEntity.setProviderMessage(notificationAttempt.getProviderMessage().name());
+        notificationAttemptEntity.setProviderErrorCode(notificationAttempt.getProviderErrorCode());
+        notificationAttemptEntity.setStatus(notificationAttempt.getStatus().name());
+
+        return notificationAttemptEntity;
+    }
+
+    public NotificationAttempt toNotificationAttempt(NotificationAttemptEntity notificationAttemptEntity) {
+        return NotificationAttempt.createFromPersistence(
+                notificationAttemptEntity.getId(),
+                notificationAttemptEntity.getNotificationEntity().getId(),
+                ProviderMessage.valueOf(notificationAttemptEntity.getProviderMessage()),
+                notificationAttemptEntity.getProviderErrorCode(),
+                AuditState.valueOf(notificationAttemptEntity.getStatus()),
+                notificationAttemptEntity.getTimestampAttempt()
+        );
+
+    }
 
 }
