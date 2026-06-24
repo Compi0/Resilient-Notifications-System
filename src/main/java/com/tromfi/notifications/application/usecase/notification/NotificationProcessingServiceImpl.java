502ff9361798e3d36fc4bf762e071babc2c082b7
Diego Hernández <compi@Diegos-MacBook-Air.local>
2026-06-24 17:48:03 -0600

    Added messageSendResult to the return type of mock service and added permanent failure case

--- src/main/java/com/tromfi/notifications/application/usecase/notification/NotificationProcessingServiceImpl.java
+++ src/main/java/com/tromfi/notifications/application/usecase/notification/NotificationProcessingServiceImpl.java
@@ -1,8 +1,12 @@
 package com.tromfi.notifications.application.usecase.notification;
 
 import com.tromfi.notifications.application.ports.out.MessageSender;
+import com.tromfi.notifications.application.ports.out.NotificationAttemptRepository;
 import com.tromfi.notifications.application.ports.out.NotificationRepository;
+import com.tromfi.notifications.application.ports.out.MessageSendResult;
 import com.tromfi.notifications.domain.model.Notification;
+import com.tromfi.notifications.domain.model.NotificationAttempt;
+import com.tromfi.notifications.domain.model.enums.AuditState;
 import com.tromfi.notifications.domain.service.ProcessNotificationService;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
@@ -16,12 +20,12 @@ public class NotificationProcessingServiceImpl implements NotificationProcessing
     private final ProcessNotificationService processNotificationService;
     private final NotificationRepository notificationRepository;
     private final MessageSender mockMessageService;
+    private final NotificationAttemptRepository notificationAttemptRepository;
 
     @Override
     @Async(value = "backgroundTaskExecutor")
     @Transactional // Aqui falta poner toda la configuracion
     public void processNotification(Notification notification) {
-
 
         boolean savedId = processNotificationService.validateNotification(notification);
 
@@ -29,12 +33,14 @@ public class NotificationProcessingServiceImpl implements NotificationProcessing
             notification.markFailed(); // Se debe de guardar
             processNotificationService.sendToDeadQueue(notification);
             notificationRepository.save(notification);
-
+            System.out.println(Thread.currentThread().getName() + ": Failed Processing Notification");
             // Tener cuidado con esta excepcion porque puede generar rollback sin guardar el estado actualizado
             // Por ahora solo se hace return pero en un futuro habilitamos la excepcion
             return;
             //throw new InvalidFieldsException("Invalid notification processing");
         }
+
+
 
         /*
 
@@ -55,21 +61,32 @@ public class NotificationProcessingServiceImpl implements NotificationProcessing
             return; // No tiene caso seguir porque ya fue procesado por un hilo anterior
         }
 
+        System.out.println(Thread.currentThread().getName() + ":Processing Notification");
+
         // Aqui se puede hacer el flush, pero puede ser mas complicado??
         lockedNotification.markProcessing();
         // Aqui ya se marca en processing, pero si no se hace el commit entonces no se observan cambios en la BD
         // En tiempo, real, esto es esperado o tenemos que modificarlo?
 
-        boolean isSent = mockMessageService.sendMessage(lockedNotification);
+        MessageSendResult messageSendResult = mockMessageService.sendMessage(lockedNotification);
+        // Pero necesito un mapper para que pase los enums
 
-        Notification processedNotification;
+        Notification processedNotification = messageSendResult.status().equals(AuditState.SUCCESS) ?
+                processNotificationService.acceptNotification(lockedNotification)
+            : processNotificationService.rejectNotification(lockedNotification);
 
-        if(!isSent){
-            processedNotification = processNotificationService.rejectNotification(lockedNotification);
-        }else{
-            processedNotification = processNotificationService.acceptNotification(lockedNotification);
+
+        NotificationAttempt notificationAttempt = NotificationAttempt.createNotificationAttempt(processedNotification.getId(),
+                messageSendResult.providerMessage(), messageSendResult.providerErrorCode(),
+                messageSendResult.status(), processedNotification.getUpdatedAt());
+
+        System.out.println(Thread.currentThread().getName() + ": Is processing" + notificationAttempt.toString());
+
+        if(messageSendResult.status() == AuditState.PERMANENT_FAILURE){
+            processedNotification.markFailed();
         }
 
+        notificationAttemptRepository.save(notificationAttempt);
         notificationRepository.save(processedNotification);
 
         // Podemos hacer alguna verificacion
